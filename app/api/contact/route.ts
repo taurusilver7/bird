1081df5bf5fa41dbc39fa0df24e895e48d2e5e29
Devi prasad <prasad20697@gmail.com>
2023-12-09 13:42:06 +0530

    email id changed

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,35 +1,35 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+export async function POST(req: Request) {
+	if (req.method === "POST") {
+		const {
+			first_name,
+			last_name,
+			email,
+			phone_number,
+			country,
+			company_size,
+			info,
+		} = await req.json();
 
-export async function POST(req: Request,) {
-  if (req.method === 'POST') {
+		try {
+			// Create a Nodemailer transport object (configure with your email provider)
+			const transporter = nodemailer.createTransport({
+				host: "smtp.gmail.com",
+				port: 465,
+				secure: true,
+				auth: {
+					user: "pvnirmala@gmail.com",
+					pass: "9440127296",
+				},
+			});
 
-    const { first_name, last_name, email, phone_number, country, company_size, info } = await req.json();
-
-
-    try {
-      // Create a Nodemailer transport object (configure with your email provider)
-      const transporter = nodemailer.createTransport( {
- 
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth : {
-            user: 'tasicigor123@gmail.com',
-            pass: 'qdhxmcvfonmldrqv'
-        }
-    } );
-    
-
-
-
-
-      const mailOptions = {
-        from: email, 
-        to: 'tasicigor123@gmail.com',
-        subject: 'Contact Form Submission',
-        html: `
+			const mailOptions = {
+				from: email,
+				to: "tasicigor123@gmail.com",
+				subject: "Contact Form Submission",
+				html: `
         <h2>Contact Form Submission</h2>
         <p><strong>First Name:</strong> ${first_name}</p>
         <p><strong>Last Name:</strong> ${last_name}</p>
@@ -40,34 +40,18 @@ export async function POST(req: Request,) {
         <p><strong>Info:</strong> ${info}</p>
 
       `,
-    };
+			};
 
+			// Send the email
+			await transporter.sendMail(mailOptions);
 
+			return NextResponse.json({ message: "Email has been sent" });
+		} catch (error) {
+			console.error(error);
 
-
-
-      // Send the email
-      await transporter.sendMail(mailOptions);
-
-    return NextResponse.json({ message: 'Email has been sent' });
-  
-    } catch (error) {
-      console.error(error);
-
-        return NextResponse.json({message: 'Error sending email'});
-
-    }
-  } else {
-    
-    return NextResponse.json({message: 'Method not allowed'});
-
-
-  }
+			return NextResponse.json({ message: "Error sending email" });
+		}
+	} else {
+		return NextResponse.json({ message: "Method not allowed" });
+	}
 }
-
-
-
-
-
-
-
